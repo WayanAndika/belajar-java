386a5423b05e1e3245e1064e2b4cf07cb6cc5000
WayanAndika <andikabuana45@gmail.com>
2023-09-28 19:33:42 -0700

    Pertemuan Ketiga

--- src/main/java/Pertemuan_3/Balok.java
+++ src/main/java/Pertemuan_3/Balok.java
@@ -8,17 +8,17 @@ package Pertemuan_3;
  *
  * @author Wayan Andika
  */
-public class Lingkaran {
+public class Balok {
+    
     public static void main(String[] args)
     {
-        int r=10;
-        double phi=3.14;
-        double kll;
-        double luas;
-        luas=phi*r*r;
-        kll= phi*r*2;
-        
-        System.out.println("Luas = "+luas);
-        System.out.println("Keliling = "+kll);
+        int p=20;
+        int l=12;
+        int t=10;
+
+        int volume=p*l*t;
+        int luasPermukaan=2*((p*l)+(p*t)+(l*t));
+        System.out.println("Volume = "+ volume);
+        System.out.println("Luas Permukaan = "+luasPermukaan);
     }
 }
