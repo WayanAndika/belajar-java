85f4a95280bd2c6dcefdff5062a2bf973663ed48
WayanAndika <andikabuana45@gmail.com>
2023-11-11 13:44:28 +0800

    Tugas | Interface

---
+++ src/main/java/Pertemuan_9/Interface/Manusia.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan_9.Interface;
+
+/**
+ *
+ * @author Lenovo
+ */
+interface Manusia {
+    public void menyanyi();
+    public void ketawa();
+}
